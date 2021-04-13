9a5b78fcff0842953b2bf36d65327427eaf1a6af
Gogs <gogs@fake.local>
2021-04-13 14:22:16 +0900

    4-5

--- app/src/main/java/com/example/fisher/Text_Content_Activity.java
+++ app/src/main/java/com/example/fisher/Text_Content_Activity.java
@@ -7,17 +7,21 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class Text_Content_Activity extends AppCompatActivity{
     private TextView text_content;
+    private Toolbar toolbar;
     private int category = 0;
     private int position = 0;
     private ImageView iContent;
     private int [] array_dog_sluj = {R.string.ovcharka, R.string.hiler, R.string.pincher, R.string.akita, R.string.malamut};
-    private int [] array_dog_decorat = {R.string.ovcharka, R.string.hiler, R.string.pincher, R.string.akita, R.string.malamut};
-    private int [] array_dog_ohot = {R.string.ovcharka, R.string.hiler, R.string.pincher, R.string.akita, R.string.malamut};
+    private int [] array_dog_decorat = {R.string.buldog, R.string.terjer, R.string.griffon, R.string.maltese, R.string.chihua};
+    private int [] array_dog_ohot = {R.string.gonchaya, R.string.koker, R.string.setter, R.string.retriver, R.string.bigl};
     private int [] array_dog_zdorov = {R.string.ovcharka, R.string.hiler, R.string.pincher, R.string.akita, R.string.malamut};
     private int [] array_image_slujeb = {R.drawable.ovch,R.drawable.hiler,R.drawable.pincher,R.drawable.akita,R.drawable.malam};
+    private int [] array_image_decor = {R.drawable.buldog,R.drawable.terier,R.drawable.griffon,R.drawable.maltese,R.drawable.chihua};
+    private int [] array_image_ohota = {R.drawable.gonchaya,R.drawable.koker,R.drawable.setter,R.drawable.retriver,R.drawable.bigl};
 
 
 
@@ -41,14 +45,18 @@ public class Text_Content_Activity extends AppCompatActivity{
         switch (category)
         {
             case 0:
+        //        toolbar.setTitle(array_dog_sluj[position]);
                 text_content.setText(array_dog_sluj[position]);
                 iContent.setImageResource(array_image_slujeb[position]);
+
                 break;
             case 1:
-
+                text_content.setText(array_dog_decorat[position]);
+                iContent.setImageResource(array_image_decor[position]);
                 break;
             case 2:
-
+                text_content.setText(array_dog_ohot[position]);
+                iContent.setImageResource(array_image_ohota[position]);
                 break;
             case 3:
 
